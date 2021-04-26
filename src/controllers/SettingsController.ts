b2fa2e9e08bc29377ef3fd00c841a38b1f5d7501
Kagi Adrian Garcia de Carvalho <kagi.carvalho@outlook.com>
2021-04-26 11:43:07 -0300

    final

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,20 +1,44 @@
-import {Request,Response} from "express"
-import {getCustomRepository} from "typeorm"
-import {SettingsRepository} from "../repositories/SettingsRepository"
+import { Request, response, Response } from 'express';
+import { SettingsService } from '../services/SettingsService';
 
 class SettingsController {
-    async create(request:Request,response:Response){
-        const {chat,username} = request.body
-        const settingsRepository = getCustomRepository(SettingsRepository);
-        const settings = settingsRepository.create({
-            chat,
-            username,
-        })
-    
-        await settingsRepository.save(settings);
-    
-        return response.json(settings)
-    }
+  async create(req: Request, res: Response) {
+    const { chat, username } = req.body;
+
+    const settingsService = new SettingsService();
+
+  try{
+    const settings = await settingsService.create({ chat, username });
+
+    return res.json(settings);
+  }catch(err) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
+  }
+  
+  async findByUsername(req: Request, res: Response) {
+    const { username } = req.params;
+
+    const settingsService = new SettingsService();
+
+    const setting = await settingsService.findByUsername(username);
+
+    return res.json(setting);
+  }
+
+  async update(req: Request, res: Response) {
+    const { username } = req.params;
+    const { chat } = req.body
+    const settingsService = new SettingsService();
+
+    const setting = await settingsService.update(username, chat);
+
+    return res.json(setting);
+  }
+
+
 }
 
-export {SettingsController}
+export { SettingsController };
--- src/entities/Setting.ts
+++ src/entities/Setting.ts
@@ -1,14 +1,15 @@
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  PrimaryColumn,
-  UpdateDateColumn,
-} from 'typeorm';
-import { v4 as uuidV4 } from 'uuid';
+  Entity, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn, 
+  PrimaryColumn } from "typeorm";
 
-@Entity('settings')
+import { v4 as uuid } from "uuid";
+
+@Entity("settings")
 class Setting {
+
   @PrimaryColumn()
   id: string;
 
@@ -16,17 +17,16 @@ class Setting {
   username: string;
 
   @Column()
-  chat: string;
-
-  @CreateDateColumn()
-  created_at: Date;
+  chat: boolean;
 
   @UpdateDateColumn()
   updated_at: Date;
 
-  constructor() {
-    if (!this.id) this.id = uuidV4();
-  }
-}
+  @CreateDateColumn()
+  created_at: Date;
 
-export { Setting };
+constructor(){
+ if(!this.id) this.id = uuid()
+ }
+}
+export {Setting}
